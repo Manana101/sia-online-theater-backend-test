3e58e052622f2d997742f3ad2a6f7d2816a1c9ad
Manana101 <manana.jaworska@gmail.com>
2020-04-27 08:20:02 +0200

    Revert "try multiple ids"
    This reverts commit 5877156d654b33c7b7a3eb86e36a7b116bc8ba98.

--- server.js
+++ server.js
@@ -30,113 +30,105 @@ const defaultPlayState = {
   timerEnd: null,
 }
 
-const id = 1
+let playState = { ...defaultPlayState }
 
-const connectPlay = (id) => {
-  const wss = new WebSocket.Server(`wss://sia-backend-test.herokuapp.com/${id}`, { port: PORT })
+const alarms = {}
 
-  let playState = { ...defaultPlayState }
-  const alarms = {}
-
-  const getEndTime = () => {
-    const now = Date.now()
-    // const minutes = playState.currentPhase === PHASES.AFTER_THE_BELL ? 10 : 20 // real times
-    const minutes = playState.currentPhase === PHASES.AFTER_THE_BELL ? 0.1 : 0.1 // demo times
-    const expiry = now + minutes * 60000
-    return expiry
-  }
-
-  const countdown = (endTime, id) => {
-    const now = Date.now()
-    const delay = endTime - now
-    if (alarms[id]) {
-      clearTimeout(alarms[id])
-    }
-    alarms[id] = setTimeout(() => {
-      wss.clients.forEach(function each(client) {
-        const event =
-          playState.currentPhase === PHASES.AFTER_THE_BELL
-            ? SOCKET_EVENTS.START_PLAY
-            : SOCKET_EVENTS.END_INTERMISSION
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(event);
-          playState.currentPhase = PHASES.SHOW_TIME
-        }
-      })
-    }, delay);
-  }
-  
-  wss.on('connection', function connection(ws) {
-    console.log('connection')
-    try {
-      ws.send(JSON.stringify(playState))
-    } catch (err) {
-      console.log('error when sending play state on connection: ', err)
-    }
-
-    ws.on('message', function incoming(event) {
-      console.log('received event: ', event);
-
-      // handle play state
-      switch(event) {
-        case SOCKET_EVENTS.CLAP:
-          playState.totalClapCount++
-          break;
-        case SOCKET_EVENTS.OPEN_CURTAIN:
-          playState.curtainOpen = true
-          break;
-        case SOCKET_EVENTS.CLOSE_CURTAIN:
-          playState.curtainOpen = false
-          break;
-        case SOCKET_EVENTS.RING_BELL:
-          playState.currentPhase = PHASES.AFTER_THE_BELL
-          playState.timerEnd = getEndTime()
-          countdown(playState.timerEnd) // set timeout to start the play
-          break;
-        case SOCKET_EVENTS.START_INTERMISSION:
-          playState.currentPhase = PHASES.INTERMISSION
-          playState.currentAct ++
-          playState.timerEnd = getEndTime()
-          countdown(playState.timerEnd) // set timeout to start act 2
-          break;
-        case SOCKET_EVENTS.END_PLAY:
-          playState.currentPhase = PHASES.AFTER_THE_SHOW
-          clearTimeout(alarms[id])
-          playState = defaultPlayState
-          break;
-      }
-
-      // send event to all clients
-      wss.clients.forEach(function each(client) {
-        // client !== ws && -> for now we want to send events back to the client for demo purposes
-        if (client.readyState === WebSocket.OPEN) {
-          switch(event) {
-            case SOCKET_EVENTS.CLAP:
-              client.send(SOCKET_EVENTS.CLAP);
-              break;
-            case SOCKET_EVENTS.OPEN_CURTAIN:
-              client.send(SOCKET_EVENTS.OPEN_CURTAIN);
-              break;
-            case SOCKET_EVENTS.CLOSE_CURTAIN:
-              client.send(SOCKET_EVENTS.CLOSE_CURTAIN);
-              break;
-            case SOCKET_EVENTS.RING_BELL:
-              client.send(SOCKET_EVENTS.RING_BELL);
-              client.send(JSON.stringify(playState));
-              break;
-            case SOCKET_EVENTS.START_INTERMISSION:
-              client.send(SOCKET_EVENTS.START_INTERMISSION);
-              client.send(JSON.stringify(playState));
-              break;
-            case SOCKET_EVENTS.END_PLAY:
-              client.send(SOCKET_EVENTS.END_PLAY);
-              break;
-          }
-        }
-      });
-    });
-  });
+const getEndTime = () => {
+  const now = Date.now()
+  // const minutes = playState.currentPhase === PHASES.AFTER_THE_BELL ? 10 : 20 // real times
+  const minutes = playState.currentPhase === PHASES.AFTER_THE_BELL ? 0.1 : 0.1 // demo times
+  const expiry = now + minutes * 60000
+  return expiry
 }
 
-connectPlay(1)
-connectPlay(2)
+const countdown = (endTime) => {
+  const now = Date.now()
+  const delay = endTime - now
+  if (alarms.playTimeout) {
+    clearTimeout(alarms.playTimeout)
+  }
+  alarms.playTimeout = setTimeout(() => {
+    wss.clients.forEach(function each(client) {
+      const event =
+        playState.currentPhase === PHASES.AFTER_THE_BELL
+          ? SOCKET_EVENTS.START_PLAY
+          : SOCKET_EVENTS.END_INTERMISSION
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(event);
+        playState.currentPhase = PHASES.SHOW_TIME
+      }
+    })
+  }, delay);
+}
+ 
+wss.on('connection', function connection(ws) {
+  console.log('connection')
+  try {
+    ws.send(JSON.stringify(playState))
+  } catch (err) {
+    console.log('error when sending play state on connection: ', err)
+  }
+
+  ws.on('message', function incoming(event) {
+    console.log('received event: ', event);
+
+    // handle play state
+    switch(event) {
+      case SOCKET_EVENTS.CLAP:
+        playState.totalClapCount++
+        break;
+      case SOCKET_EVENTS.OPEN_CURTAIN:
+        playState.curtainOpen = true
+        break;
+      case SOCKET_EVENTS.CLOSE_CURTAIN:
+        playState.curtainOpen = false
+        break;
+      case SOCKET_EVENTS.RING_BELL:
+        playState.currentPhase = PHASES.AFTER_THE_BELL
+        playState.timerEnd = getEndTime()
+        countdown(playState.timerEnd) // set timeout to start the play
+        break;
+      case SOCKET_EVENTS.START_INTERMISSION:
+        playState.currentPhase = PHASES.INTERMISSION
+        playState.currentAct ++
+        playState.timerEnd = getEndTime()
+        countdown(playState.timerEnd) // set timeout to start act 2
+        break;
+      case SOCKET_EVENTS.END_PLAY:
+        playState.currentPhase = PHASES.AFTER_THE_SHOW
+        clearTimeout(alarms.playTimeout)
+        playState = defaultPlayState
+        break;
+    }
+
+    // send event to all clients
+    wss.clients.forEach(function each(client) {
+      // client !== ws && -> for now we want to send events back to the client for demo purposes
+      if (client.readyState === WebSocket.OPEN) {
+        switch(event) {
+          case SOCKET_EVENTS.CLAP:
+            client.send(SOCKET_EVENTS.CLAP);
+            break;
+          case SOCKET_EVENTS.OPEN_CURTAIN:
+            client.send(SOCKET_EVENTS.OPEN_CURTAIN);
+            break;
+          case SOCKET_EVENTS.CLOSE_CURTAIN:
+            client.send(SOCKET_EVENTS.CLOSE_CURTAIN);
+            break;
+          case SOCKET_EVENTS.RING_BELL:
+            client.send(SOCKET_EVENTS.RING_BELL);
+            client.send(JSON.stringify(playState));
+            break;
+          case SOCKET_EVENTS.START_INTERMISSION:
+            client.send(SOCKET_EVENTS.START_INTERMISSION);
+            client.send(JSON.stringify(playState));
+            break;
+          case SOCKET_EVENTS.END_PLAY:
+            client.send(SOCKET_EVENTS.END_PLAY);
+            break;
+        }
+      }
+    });
+  });
+});
